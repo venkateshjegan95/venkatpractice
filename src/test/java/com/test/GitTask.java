1662192b2d12107a080ae12d9f850afd15ae75d2
venkateshjegan95 <venkateshjeganathan95@gmail.com>
2022-11-12 19:15:24 +0530

    add new

--- src/test/java/com/test/GitTask.java
+++ src/test/java/com/test/GitTask.java
@@ -1,5 +1,14 @@
 package com.test;
 
 public class GitTask {
+	public void method1() {
+	System.out.println("1");
+}
+	public void method2() {
+	System.out.println("2");
+}
+	public void method3() {
+		System.out.println("3");
+	}
 
 }
